63879092b5bcdaf9e7e91913ca342e186e1fff4a
Miguel Serrano <mlsv89@gmail.com>
2016-07-15 12:53:23 +0200

    fixed memory issues with tripadvisor scraper

--- workers/tripadvisor/tripadvisor_list.js
+++ workers/tripadvisor/tripadvisor_list.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const database = require('mssql');
 const _ = require('lodash');
 const xray = require('x-ray');
 
@@ -16,65 +15,78 @@ const size = 30;
 
 // Set up input data
 
-const input = JSON.parse(fs.readFileSync(path.join(__dirname, './input/cities.json')))
-  .map((item) => Object.assign(item, {
-    name: item.name,
-    cluster: item.id,
-    section: null,
-  }));
+const dbConfig = {
+  user: process.env.CSADB_USER,
+  password: process.env.CSADB_PASSWORD,
+  server: process.env.CSADB_SERVER,
+  database: process.env.CSADB_DATABASE,
+};
+database.connect(dbConfig)
+.then(() => database.query`
+  SELECT *
+  FROM ibc_seg.DM_SOURCE_TRIPADVISOR_PAGES_RAW`)
+.then((rows) => {
+  const input = rows
+    .map((item) => Object.assign(item, {
+      name: item.id,
+      cluster: item.id,
+      section: null,
+    }));
 
-// Set up handlers
+  // Set up handlers
 
-function handleGet({ url }) {
-  const x = xray();
+  function handleGet({ url }) {
+    const x = xray();
 
-  return new Promise((resolve, reject) => {
-    const scrape = x(url, '#EATERY_SEARCH_RESULTS div.listing',
-      [{
-        id: '@id',
-        url: '.property_title@href',
-        name: '.property_title',
-        rank: '.popIndex',
-        price: '.price_range',
-        cuisine: ['.cuisine'],
-      }]
-    ).paginate('.nav.next@href');
+    return new Promise((resolve, reject) => {
+      const scrape = x(url, '#EATERY_SEARCH_RESULTS div.listing',
+        [{
+          id: '@id',
+          url: '.property_title@href',
+          name: '.property_title',
+          rank: '.popIndex',
+          price: '.price_range',
+          cuisine: ['.cuisine'],
+        }]
+      );
 
-    scrape((err, arr) => {
-      if (err) reject(err);
-      else resolve(arr);
-    });
-  })
-  .catch(error => ({ error, source: 'handleGet' }));
-}
-
-function handleResponse(item, response, done) {
-  const { cluster, section } = item;
-  const datetime = new Date().toISOString();
-
-  return response
-    .map((row) => {
-      // last opportunity to modify response objects
-      const newRow = row;
-
-      newRow.name = row.name.replace('\n', '').trim();
-      newRow.rank = row.rank ? row.rank.replace('\n', '').trim() : null;
-      newRow.city = item.label;
-      newRow.cuisine = row.cuisine ? row.cuisine.join(', ') : null;
-
-      return newRow;
+      scrape((err, arr) => {
+        if (err) reject(err);
+        else resolve(arr);
+      });
     })
-    .map((row, index) => _.merge({}, model, row, { cluster, section, index, datetime }))
-    .filter(row => done.indexOf(row.id.toString()) === -1)
-    .filter((row, index, array) => array.slice(0, index).map(row => row.id).indexOf(row.id) === -1);
+    .catch(error => ({ error, source: 'handleGet' }));
+  }
 
-  // return { error: response.meta, source: 'handleResponse' };
-}
+  function handleResponse(item, response, done) {
+    const { cluster, section } = item;
+    const datetime = new Date().toISOString();
 
-// Run
+    return response
+      .map((row) => {
+        // last opportunity to modify response objects
+        const newRow = row;
 
-run({
-  config: { origin, list, size },
-  data: { input, model },
-  handlers: { handleGet, handleResponse },
-});
+        newRow.name = row.name.replace('\n', '').trim();
+        newRow.rank = row.rank ? row.rank.replace('\n', '').trim() : null;
+        newRow.city = item.label;
+        newRow.cuisine = row.cuisine ? row.cuisine.join(', ') : null;
+
+        return newRow;
+      })
+      .map((row, index) => _.merge({}, model, row, { cluster, section, index, datetime }))
+      .filter(row => done.indexOf(row.id.toString()) === -1)
+      .filter((row, index, array) => array.slice(0, index).map(row => row.id).indexOf(row.id) === -1);
+
+    // return { error: response.meta, source: 'handleResponse' };
+  }
+
+  // Run
+
+  run({
+    config: { origin, list, size },
+    data: { input, model },
+    handlers: { handleGet, handleResponse },
+  });
+})
+.catch(err => console.log(err));
---
+++ workers/tripadvisor/tripadvisor_pages.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const xray = require('x-ray');
+
+const run = require('../runner.js');
+const model = require('./model_pages.js');
+
+// Set up config
+
+const origin = 'tripadvisor';
+const list = 'pages';
+const size = 1;
+
+// Set up input data
+
+const input = JSON.parse(fs.readFileSync(path.join(__dirname, './input/cities.json')))
+  .map((item) => Object.assign(item, {
+    name: item.name,
+    cluster: item.id,
+    section: null,
+  }));
+
+// Set up handlers
+
+function handleGet({ id, url }) {
+  const x = xray();
+
+  return new Promise((resolve, reject) => {
+    const scrape = x(url, '.pageNumbers', ['a']);
+
+    scrape((err, arr) => {
+      if (err) reject(err);
+      else {
+        const urls = [];
+        const pages = parseInt(arr[arr.length - 1], 10);
+        for (let i = 0; i < pages; i++) {
+          urls.push(`https://www.tripadvisor.es/RestaurantSearch?geo=${id}&o=a${i * 30}&itags=10591&sortOrder=popularity`);
+        }
+        resolve(urls);
+      }
+    });
+  })
+  .catch(error => ({ error, source: 'handleGet' }));
+}
+
+function handleResponse(item, response, done) {
+  const { cluster, section } = item;
+  const datetime = new Date().toISOString();
+
+  return response
+    .map((row) => {
+      // last opportunity to modify response objects
+      const newRow = { url: row };
+
+      newRow.id = row.replace('https://www.tripadvisor.es/RestaurantSearch?', '');
+
+      return newRow;
+    })
+    .map((row, index) => _.merge({}, model, row, { cluster, section, index, datetime }))
+    .filter(row => done.indexOf(row.id.toString()) === -1);
+
+  // return { error: response.meta, source: 'handleResponse' };
+}
+
+// Run
+
+run({
+  config: { origin, list, size },
+  data: { input, model },
+  handlers: { handleGet, handleResponse },
+});
