7c05d1a71a75a7c9e1f053df1a2f447ce076f1a2
Miguel Serrano <mlsv89@gmail.com>
2016-07-27 19:33:26 +0200

    replaced static cetroids with foursquare db points

--- workers/yelp/yelp_venues.js
+++ workers/yelp/yelp_venues.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
+const database = require('mssql');
 const _ = require('lodash');
 const Yelp = require('yelp');
 
@@ -22,75 +21,93 @@ const apiConfig = {
 
 // Set up input data
 
-const input = JSON.parse(fs.readFileSync(path.join(__dirname, './input/centroides.json')))
-  .map((item) => Object.assign({ latlon: item }, {
-    offset: 0,
-    name: item,
-    cluster: item,
-    section: 'restaurants,nightlife',
-  }));
+const dbConfig = {
+  user: process.env.CSADB_USER,
+  password: process.env.CSADB_PASSWORD,
+  server: process.env.CSADB_SERVER,
+  database: process.env.CSADB_DATABASE,
+};
 
-// Set up handlers
+database.connect(dbConfig)
+.then(() => database.query`
+  SELECT [location.lat] AS [lat], [location.lng] AS [lng]
+  FROM [ibc_seg].[DM_SOURCE_FOURSQUARE_VENUES_RAW]
+  WHERE [location.lat] IS NOT NULL AND [location.lng] IS NOT NULL
+  GROUP BY [location.lat], [location.lng]`
+)
+.then((values) => {
+  const input = values
+    .map(item => `${item.lat},${item.lng}`)
+    .map((item) => Object.assign({ latlon: item }, {
+      offset: 0,
+      name: item,
+      cluster: item,
+      section: 'restaurants,nightlife',
+    }));
 
-function handleGet({ offset, latlon, section }) {
-  const yelp = new Yelp(apiConfig);
-  return yelp.search({
-    offset,
-    limit: 20,
-    ll: latlon,
-    category_filter: section,
-    radius_filter: 150,
-    sort: 1,
-  })
-  .catch(error => ({ error, source: 'handleGet' }));
-}
+  // Set up handlers
 
-function handleResponse(item, response, done) {
-  const { cluster, section } = item;
-  const datetime = new Date().toISOString();
-
-  if (!response.statusCode) {
-    const pages = [];
-    let numPages = 1;
-
-    if (response.total > 20 && item.offset === 0) {
-      numPages = Math.ceil(response.total / 20);
-
-      for (let i = 1; i < numPages; i++) {
-        const temp = Object.assign({}, item);
-        temp.offset = (i * 20) + 1;
-        pages.push(temp);
-      }
-    }
-
-    const rows = response.businesses
-      .filter(row => done.indexOf(row.id.toString()) === -1);
-
-    if (rows.length) {
-      const result = rows
-        .map((row) => {
-          // last opportunity to modify response objects
-          const newRow = row;
-          return newRow;
-        })
-        .map((row, index) => _.merge({}, model, row, { cluster, section, index, datetime }));
-
-      if (numPages > 1) return { result, pages };
-      return result;
-    }
-
-    if (numPages > 1) return { result: [], pages };
-    const id = `empty_centroid (${cluster})`;
-    return [_.merge({}, model, { id, cluster, section, index: null, datetime })];
+  function handleGet({ offset, latlon, section }) {
+    const yelp = new Yelp(apiConfig);
+    return yelp.search({
+      offset,
+      limit: 20,
+      ll: latlon,
+      category_filter: section,
+      radius_filter: 150,
+      sort: 1,
+    })
+    .catch(error => ({ error, source: 'handleGet' }));
   }
 
-  return { error: response.meta, source: 'handleResponse' };
-}
+  function handleResponse(item, response, done) {
+    const { cluster, section } = item;
+    const datetime = new Date().toISOString();
 
-// Run
+    if (!response.statusCode) {
+      const pages = [];
+      let numPages = 1;
 
-run({
-  config: { origin, list, size },
-  data: { input, model },
-  handlers: { handleGet, handleResponse },
-});
+      if (response.total > 20 && item.offset === 0) {
+        numPages = Math.ceil(response.total / 20);
+
+        for (let i = 1; i < numPages; i++) {
+          const temp = Object.assign({}, item);
+          temp.offset = (i * 20) + 1;
+          pages.push(temp);
+        }
+      }
+
+      const rows = response.businesses
+        .filter(row => done.indexOf(row.id.toString()) === -1);
+
+      if (rows.length) {
+        const result = rows
+          .map((row) => {
+            // last opportunity to modify response objects
+            const newRow = row;
+            return newRow;
+          })
+          .map((row, index) => _.merge({}, model, row, { cluster, section, index, datetime }));
+
+        if (numPages > 1) return { result, pages };
+        return result;
+      }
+
+      if (numPages > 1) return { result: [], pages };
+      const id = `empty_centroid (${cluster})`;
+      return [_.merge({}, model, { id, cluster, section, index: null, datetime })];
+    }
+
+    return { error: response.meta, source: 'handleResponse' };
+  }
+
+  // Run
+
+  run({
+    config: { origin, list, size },
+    data: { input, model },
+    handlers: { handleGet, handleResponse },
+  });
+})
+.catch(err => console.log(err));
